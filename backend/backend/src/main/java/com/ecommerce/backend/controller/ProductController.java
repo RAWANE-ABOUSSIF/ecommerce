f333c3c604b36a3dcde40010f2280cf3f56192e7
RAWANE-ABOUSSIF <rawane.abou@gmail.com>
2025-07-21 17:57:25 +0200

    Ajout du projet complet

---
+++ backend/backend/src/main/java/com/ecommerce/backend/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.ecommerce.backend.controller;
+
+import com.ecommerce.backend.entity.Product;
+import com.ecommerce.backend.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:3000") // autorise les appels du frontend React
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+}
